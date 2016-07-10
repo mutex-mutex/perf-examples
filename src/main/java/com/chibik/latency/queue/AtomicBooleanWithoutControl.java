5f27d05fa19a7854e5e404efdae81e89d526109e
chibik-mutex <cheboksarov@phystech.edu>
2016-07-10 18:10:16 +0300

    added first benchmarks

---
+++ src/main/java/com/chibik/latency/queue/AtomicBooleanWithoutControl.java
@@ -0,0 +1,47 @@
+package com.chibik.latency.queue;
+
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.infra.Control;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+@State(Scope.Group)
+public class AtomicBooleanWithoutControl {
+
+    public final AtomicBoolean flag = new AtomicBoolean();
+
+    @Benchmark
+    @Group("pingpong")
+    public void ping() {
+        while (!flag.compareAndSet(false, true)) {
+
+        }
+    }
+
+    @Benchmark
+    @Group("pingpong")
+    public void pong() {
+        while (!flag.compareAndSet(true, false)) {
+
+        }
+    }
+
+    public static void main(String[] args) throws RunnerException {
+        Options opt = new OptionsBuilder()
+                .include(AtomicBooleanWithoutControl.class.getSimpleName())
+                .warmupIterations(1)
+                .measurementIterations(5)
+                .threads(2)
+                .forks(1)
+                .mode(Mode.AverageTime)
+                .timeUnit(TimeUnit.NANOSECONDS)
+                .build();
+
+        new Runner(opt).run();
+    }
+}
